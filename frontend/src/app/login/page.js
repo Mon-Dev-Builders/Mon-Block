62ac6731e60a14aba0071996836723ef11d3ba37
monblockdev <monblock.dev12@gmail.com>
2024-08-15 21:11:01 +0800

    login start

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,13 +1,13 @@
 import { Footer, Header, LoginContent } from "@/components";
 
 export default function Login() {
-  return (
-    <>
-      <div className="shadow-2xl w-fit m-auto">
-        <Header />
-        <LoginContent />
-        <Footer />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="shadow-2xl w-fit m-auto">
+                <Header />
+                <LoginContent />
+                <Footer />
+            </div>
+        </>
+    )
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,9 +3,10 @@ import { Container } from "./assets";
 export const LoginContent = () => {
   return (
     <Container>
-      <div className="flex h-[650px] justify-center items-center">
-        <div className="text-7xl max-w-[700px] text-center">
-          Онлайн худалдаа тун удахгүй...
+      <div className="flex w-[557px] m-auto bg-green-200 justify-center items-center">
+        <div className="flex flex-col">
+          <label htmlFor="">Нэвтрэх</label>
+          <input type="text" className="px-4 w-[300px]" placeholder="И-мэйл хаягаа оруулна уу" />
         </div>
       </div>
     </Container>
