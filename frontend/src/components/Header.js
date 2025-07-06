b64d3eb982b8922e3e756f6a87b8de5551444e0c
Benord <Baynaabj749@gmail.com>
2025-07-06 12:00:45 +0800

    logo update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -23,13 +23,13 @@ export const Header = () => {
   const router = useRouter();
   return (
     <Container>
-      <div className="xl:shadow-2xl w-full bg-white pb-8  pt-8 xl:pt-16  px-8 xl:px-24 relative z-50">
+      <div className="relative z-50 w-full px-8 pt-8 pb-8 bg-white xl:shadow-2xl xl:pt-16 xl:px-24">
         <div className="max-w-[1631px] m-auto flex justify-between items-center text-2xl">
           <Link href="/">
             <div
-              className="w-44 sm:w-60 xl:w-72  h-9 sm:h-12 xl:h-14"
+              className="w-44 sm:w-60 xl:w-72 h-9 sm:h-12 xl:h-14"
               style={{
-                backgroundImage: `url("https://res.cloudinary.com/dzm85pldh/image/upload/v1723290540/Logo_iueel9.png")`,
+                backgroundImage: `url("headLogo.png")`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
@@ -56,7 +56,7 @@ export const Header = () => {
                   ? "#C81127"
                   : "#002672",
             }}
-            className="py-1 px-8 xl:px-10 text-sm xl:text-base h-fit hidden sm:flex  text-white rounded-2xl"
+            className="hidden px-8 py-1 text-sm text-white xl:px-10 xl:text-base h-fit sm:flex rounded-2xl"
           >
             Нэвтрэх
           </button>
