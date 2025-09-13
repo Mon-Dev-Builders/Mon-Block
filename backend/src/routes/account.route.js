5f625e61bbeda3dd9de6bf2b13a642ab53676ba5
Rdnprotocol <monblock.dev@gmail.com>
2025-09-14 06:45:06 +0800

    delete backend

--- backend/src/routes/account.route.js
+++
@@ -1,5 +0,0 @@
-const { Router } = require("express");
-
-const accountRouter = Router();
-
-module.exports = { accountRouter };
--- backend/src/routes/product.route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const {
-  getAllProducts,
-  createProduct,
-} = require("../controllers/product.controller");
-
-const productRouter = Router();
-productRouter.get("/", getAllProducts).post("/", createProduct);
-module.exports = { productRouter };
